df72786f35d532296bca579a23f15eda9d1c2711
shaikhsafiurrahman16 <shaikhsafiurrahman16@gmail.com>
2026-07-19 00:34:45 +0500

    Add Fixes

--- config/db.js
+++ config/db.js
@@ -1,17 +1,30 @@
-const mysql = require('mysql2/promise');
-const dotenv = require('dotenv');
+// const mysql = require('mysql2/promise');
+// const dotenv = require('dotenv');
 
-dotenv.config();
+// dotenv.config();
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST || 'localhost',
+//   user: process.env.DB_USER || 'root',
+//   password: process.env.DB_PASSWORD || 'Safi123.' ,
+//   database: process.env.DB_NAME || 'papertech',
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+//   connectTimeout: 10000,
+// });
+
+// module.exports = pool;
+
+
+const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD ? process.env.DB_PASSWORD.replace(/^"|"$/g, '') : '',
-  database: process.env.DB_NAME || 'papertech',
+  uri: 'mysql://root:WqbSKxrhzwyIXQEhEUBmxCLjanbMnlnj@tokaido.proxy.rlwy.net:14397/railway',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  connectTimeout: 10000,
+  connectTimeout: 15000,
 });
 
 module.exports = pool;
