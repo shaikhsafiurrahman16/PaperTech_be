fbc2ada491a3cee3fbb252d6415c06234fe3ae3f
shaikhsafiurrahman16 <shaikhsafiurrahman16@gmail.com>
2026-07-19 13:37:56 +0500

    Add fixes

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 // const mysql = require('mysql2/promise');
-// const dotenv = require('dotenv');
+const dotenv = require('dotenv');
 
-// dotenv.config();
+dotenv.config();
 
 // const pool = mysql.createPool({
 //   host: process.env.DB_HOST || 'localhost',
@@ -20,7 +20,7 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  uri: 'mysql://root:WqbSKxrhzwyIXQEhEUBmxCLjanbMnlnj@tokaido.proxy.rlwy.net:14397/railway',
+  uri:  process.env.LIVE_DB || 'mysql://root:WqbSKxrhzwyIXQEhEUBmxCLjanbMnlnj@tokaido.proxy.rlwy.net:14397/railway',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
