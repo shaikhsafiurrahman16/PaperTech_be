b8bc8a7c78f5a9353d599336d8443b07d72bd245
shaikhsafiurrahman16 <shaikhsafiurrahman16@gmail.com>
2026-07-24 11:39:09 +0500

    Add Fixes

--- config/db.js
+++ config/db.js
@@ -1,30 +1,25 @@
-// const mysql = require('mysql2/promise');
+const mysql = require('mysql2/promise');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST || 'localhost',
-//   user: process.env.DB_USER || 'root',
-//   password: process.env.DB_PASSWORD || 'Safi123.' ,
-//   database: process.env.DB_NAME || 'papertech',
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-//   connectTimeout: 10000,
-// });
-
-// module.exports = pool;
-
-
-const mysql = require('mysql2/promise');
-
 const pool = mysql.createPool({
-  uri:  process.env.LIVE_DB || 'mysql://root:WqbSKxrhzwyIXQEhEUBmxCLjanbMnlnj@tokaido.proxy.rlwy.net:14397/railway',
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'Safi123.' ,
+  database: process.env.DB_NAME || 'papertech',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  connectTimeout: 15000,
+  connectTimeout: 10000,
 });
+
+// const pool = mysql.createPool({
+//   uri:  process.env.LIVE_DB || 'mysql://root:WqbSKxrhzwyIXQEhEUBmxCLjanbMnlnj@tokaido.proxy.rlwy.net:14397/railway',
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+//   connectTimeout: 15000,
+// });
 
 module.exports = pool;
